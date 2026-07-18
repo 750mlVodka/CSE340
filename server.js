44111984d97a090f7d135d67e82a3da77d9ac06c
Yamsir Hernandez <yamsirgg@gmail.com>
2026-07-17 19:40:17 -0600

    feat: implement database connectivity with PostgreSQL and create foundational EJS views for the application

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
+import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { testConnection } from './src/models/db.js';
+
+// Define the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production'
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
 
+// Ser EJS as the templating engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
 });
@@ -29,6 +47,14 @@ app.get('/categories', (req, res) => {
   res.render('categories', { title: 'Categories' });
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, async() => {
+
+  try {
+    await testConnection();
+    console.log(`Server listening at http://localhost:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error(`Failed to start server: ${error.message}`);
+    process.exit(1);
+  }
 });
